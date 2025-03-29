237d0c8c8cae6f95fd35892c674c16ea3356ca1c
kawaiicakes <vomitmopper@gmail.com>
2025-03-29 14:55:29 -0600

    removing example placeholder shit

---
+++ common/src/main/java/io/github/kawaiicakes/create_cutil/CreateCollisionUtil.java
@@ -0,0 +1,19 @@
+package io.github.kawaiicakes.create_cutil;
+
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CreateCollisionUtil {
+    public static final String MOD_ID = "create_cutil";
+    public static final String NAME = "Create: Control Utilities";
+    public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+
+    public static void init() {
+        CreateCollisionUtilBlocks.init(); // hold registrate in a separate class to avoid loading early on forge
+    }
+
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
+}
